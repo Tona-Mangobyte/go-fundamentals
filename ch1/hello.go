fd51f4df3348b1529e3c36c7d1e122c50d9ee897
Tona <tona@mango-byte.com>
2024-07-14 20:40:43 +0700

    add variables

--- ch1/hello.go
+++ ch1/hello.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
+
 	fmt.Println("Hello, world!")
+
+	var x int = 5
+	y := 7
+	sum := x + y
+	fmt.Println(sum)
+
+	const z int = 9
+	// z = 10 // error
+	fmt.Println(z)
+
 }
